b3b65f33a65d17e1f8ce5366a05be770b4edac46
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-19 00:24:53 +0500

    client config

---
+++ client/src/main.rs
@@ -0,0 +1,12 @@
+use std::io::{self, ErrorKind, Read, Write};
+use std::sync::mpsc::{self, TryRecvError};
+use std::net::TcpStream;
+use std::time::Duration;
+use std::thread;
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+fn main() {
+    let mut 
+}
